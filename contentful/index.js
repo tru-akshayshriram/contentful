125fb220a0d10f63daa0ab1e3e5f8cdd06201b52
tru-akshayshriram <ashriram@truperformance.us>
2024-12-27 19:22:17 +0530

    Contentful App

---
+++ contentful/index.js
@@ -0,0 +1,35 @@
+const fs = require("fs");
+const { createClient } = require("contentful");
+
+const client = createClient({
+    space: '0ww4nq80qzhz',
+    environment: 'master', // defaults to 'master' if not set
+    accessToken: 'ppVCGuYcgahkA05uculaqmwuGg8qfys-sCtwmcgTWFc',
+    // host: 'cdn.contentful.com' //Delivery API (Published Content)
+    // host: 'preview.contentful.com' //Preview API
+})
+
+client.getEntries()
+    .then((data) => {
+        console.log("data", data);
+
+        const [siteEntry] = data.items.filter(
+            (entry) => entry.sys.contentType.sys.id === 'site'
+        );
+
+        // JSON to string
+        const JSONstring = JSON.stringify(siteEntry, null, 2);
+
+        const filePath = "data.json";
+
+        fs.writeFile(filePath, JSONstring, (err) => {
+            if (err) {
+                console.error("Failed to save the JSON data:", err);
+            } else {
+                console.log("JSON data saved in the file:", filePath);
+
+            }
+        })
+    })
+    .catch(console.error)
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,113 +1,53 @@
 import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import data from '../data.json'
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 
 export default function Home() {
+  console.log("data", data);
+  const { fields: { title, footer, body, banner: { fields: { file: { url: imageUrl }, title: imageTitle } }, products } } = data;
+
   return (
     <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
+      className={` grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 `}
     >
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/pages/index.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+        <div className="relative font-sans ">
+          <Image src={`https:${imageUrl}`} alt={imageTitle} className=" inset-0 h-fit w-fit lg:w-screen lg:h-screen object-contain" width={1000} height={1000} />
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          <div className="min-h-[350px] relative z-50 h-full max-w-6xl mx-auto flex flex-col justify-center items-center text-center text-white p-6">
+            <h2 className="sm:text-4xl text-2xl font-bold mb-6">{title}</h2>
+            <div>{documentToReactComponents(body)}</div>
+            <p className="sm:text-lg text-base text-center text-gray-200">Embark on unforgettable journeys. Book your dream vacation today!</p>
+
+          </div>
+        </div>
+        <div className="productsRow flex flex-wrap -m-2 ">
+          {console.log(products)}
+          {products.map(({ sys: { id: productID },
+            fields: { description,
+              image: { fields: {
+                file: { url }, title } },
+              imei,
+              name,
+              tags } }) => (
+            <div key={productID} className="w-full md:w-1/2 lg:w-1/3 px-2">
+              <div className="flex flex-col gap-4">
+                <Image src={`https:${url}`} alt={title} width={300} height={400} style={{ borderRadius: "30px" }} />
+                <h3>MODEL: {name}</h3>
+                <p>IMEI: {imei}</p>
+                <div className="grid grid-cols-3 gap-4">
+                  {tags.map(({ sys: { id: tagId }, fields: { name: tagsName } }) => (
+                    <p key={tagId} className="rounded-full bg-indigo-300 text-black p-2 text-center">{tagsName}</p>
+                  ))}
+                </div>
+                <div>{documentToReactComponents(description)}</div>
+              </div>
+            </div>
+          ))}
         </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+        {footer}
       </footer>
     </div>
   );
